321e0c456dfa1035c5dcdc09f2d76bc23f7360dc
Igor Dianov <igdianov@users.noreply.github.com>
2019-10-24 20:36:05 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "node-app-terraform"
+    bucket = "node-app-s3-terraform"
     key = "terraform.tfstate"
     region = "ap-southeast-2"
   }
@@ -9,7 +9,7 @@ terraform {
 data "terraform_remote_state" "network" {
   backend = "s3"
   config {
-    bucket = "node-app-terraform"
+    bucket = "node-app-s3-terraform"
     key    = "terraform.tfstate"
     region = "ap-southeast-2"
   }
